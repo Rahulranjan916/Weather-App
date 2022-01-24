6edfef219f2b16a6218e1f7453ec1fee6330dcfb
Rahulranjan916 <rahulranjan91622@gmail.com>
2022-01-24 18:21:38 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React,{ useState, useEffect } from 'react';
+import Weather from './components/Weather';
+import SearchBar from './components/SearchBar';
+
+
+ const API_KEY = "3b38561e4950edb6e081c31e65b0d0c6";
 
 export default function App() {
+  const[weatherData, setWeatherData] = useState(null);
+  const[loaded, setLoaded] = useState(true);
+
+  async function fetchWeatherData(cityName){
+    setLoaded(false);
+    const API=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+    try{
+      const response = await fetch(API);
+      if(response.status == 200)
+      {
+        const data = await response.json();
+        setWeatherData(data);
+      }
+      else{
+        setWeatherData(null);
+      }
+      setLoaded(true);
+    }
+    catch(error)
+    {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchWeatherData('Goa');
+    console.log(weatherData);
+  }, [])
+
+  if(!loaded)
+  {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator color='gray'
+        size ={36}/>
+      </View>
+    )
+  }
+
+  else if(weatherData == null)
+  {
+    console.log("Rahul")
+    return(
+      
+    <View>
+    <SearchBar fetchWeatherData={fetchWeatherData} />
+    <Text style={styles.primaryText}>City Not found! Try different City</Text>
+    </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData}/>
+        </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignContent:'center',
+    alignItems:'center',
+    marginTop:50
   },
-});
+  primaryText:{
+    margin:20,
+    fontSize:28
+  }
+})
---
+++ assets/Background/index.js
@@ -0,0 +1,6 @@
+const haze =require('./Image/haze.jpg');
+const rainy = require('./Image/rainy.jpg');
+const snow = require('./Image/snow.jpg');
+const sunny = require('./Image/sunny.jpg');
+
+export default(haze, rainy, snow, sunny);
